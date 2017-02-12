d6bc3d31623a1f49b8503c85cb0c7fe79b5ba24e
Aaron Hampton <aaron.hampt@gmail.com>
2017-02-11 20:59:29 -0500

    Bumping version and updating Makefile

--- Makefile
+++ Makefile
@@ -2,6 +2,5 @@ clean:
 	rm -rf build dist littleblog.egg*
 build: clean
 	python setup.py sdist bdist_wheel
-upload:
+upload: build
 	twine upload dist/*
-	clean
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='littleblog',
-    version='0.3.0',
+    version='0.3.1',
     description='littleblog - a simple static site generator',
     url='https://github.com/wookalar/littleblog',
-    author='Wookalar Software',
-    author_email='doc.tart@wookalar.io',
+    author='Aaron Hampton',
+    author_email='aaron.hampt@gmail.com',
     license='AGPLv3',
     package_data={
         'littleblog': [
@@ -15,7 +15,7 @@ setup(
     install_requires=[
         'click>=6.6',
         'Jinja2>=2.8',
-        'Markdown>=2.6.7',
+        'Markdown>=2.6.8',
         'MarkupSafe>=0.23',
         'Pygments>=2.1.3',
     ],
